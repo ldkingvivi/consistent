04452581e3b9574ae2135a7042399c7335e5e1a9
Di Li <di.li@bytedance.com>
2023-08-16 13:04:34 -0700

    update test

--- example/partcal_test.go
+++ example/partcal_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/buraksezer/consistent"
 	"log"
+	"strconv"
 	"testing"
 )
 
@@ -102,4 +103,57 @@ func Test_MisMemberMapping(t *testing.T) {
 			t.Errorf("not consistent")
 		}
 	}
+}
+
+func Test_PatitionConsistent(t *testing.T) {
+	// new with []100 and remove 1, if this is the same as new with []99, pick a middle one to remove
+
+	// create baseline
+	members := []consistent.Member{}
+	for i := 0; i < 108; i++ {
+		if i == 5 || i == 13 || i == 98 {
+			continue
+		}
+
+		member := Member(fmt.Sprintf("node%d.olricmq", i))
+		members = append(members, member)
+	}
+
+	pCount := 6113
+
+	cfg := consistent.Config{
+		PartitionCount:    pCount,
+		ReplicationFactor: 40,
+		Load:              1.0000001,
+		Hasher:            hasher{},
+	}
+
+	c := consistent.New(members, cfg)
+	mapping := make(map[int]int)
+	for i := 0; i < 10000; i++ {
+		p := c.FindPartitionID([]byte(strconv.Itoa(i)))
+		mapping[i] = p
+	}
+
+	for s, p := range mapping {
+		log.Printf("%d map to %d\n", s, p)
+	}
+
+	// create nw one
+	newM := []consistent.Member{}
+	for j := 0; j < 1; j++ {
+		member := Member(fmt.Sprintf("node%d.olricmq", j))
+		newM = append(newM, member)
+	}
+	newC := consistent.New(newM, cfg)
+
+	for i := 0; i < 10000; i++ {
+		p := newC.FindPartitionID([]byte(strconv.Itoa(i)))
+
+		mP := mapping[i]
+		if p != mP {
+			t.Errorf("not consistent")
+		}
+	}
+
 }
